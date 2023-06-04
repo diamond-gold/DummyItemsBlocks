e2a2d6ecbe8ee242374573eab7199d9fdf3496dd
ace <diamond-gold-ace@outlook.com>
2023-06-04 18:14:44 +0800

    Upload

---
+++ README.md
@@ -0,0 +1,25 @@
+![DummyItemsBlocks](https://socialify.git.ci/diamond-gold/DummyItemsBlocks/image?description=1&font=Inter&forks=1&issues=1&name=1&owner=1&pattern=Circuit%20Board&pulls=1&stargazers=1&theme=Light)
+
+> Items and blocks added are not functional, they are just for decoration purposes.
+> 
+> Also allows worlds with such items or blocks to be loaded
+
+Encountered a bug? Want something added? Create an [issue](https://github.com/diamond-gold/DummyItemsBlocks/issues) (＃°Д°)
+
+Have questions? Start a [discussion](https://github.com/diamond-gold/DummyItemsBlocks/discussions) ✍(◔◡◔)
+
+Starring the GitHub repository is like sprinkling stardust—help me create magic! (。・∀・)ノ
+
+Feeling generous? Buy me some [snacks](https://ko-fi.com/diamondgold)! (❤´艸｀❤)
+
+[![Feature Requests](https://img.shields.io/github/issues-raw/diamond-gold/DummyItemsBlocks/Feature%20Request?label=Feature%20Requests&logo=github&style=for-the-badge)](https://github.com/diamond-gold/DummyItemsBlocks/issues)
+[![Bug Reports](https://img.shields.io/github/issues-raw/diamond-gold/DummyItemsBlocks/bug?label=Bug%20Reports&logo=github&style=for-the-badge)](https://github.com/diamond-gold/DummyItemsBlocks/issues)
+
+# Known issue
+`
+[Server thread/CRITICAL]: InvalidArgumentException: "Deserializer is already assigned for "minecraft:shield"" (EXCEPTION) in "pmsrc/src/data/bedrock/item/ItemDeserializer" at line 55
+`
+
+> If you encounter similar error in another plugin, please delete `minecraft:shield` in `config.yml`
+> 
+> Replace `minecraft:shield` with what you see in the error message
---
+++ src/diamondgold/DummyItemsBlocks/Main.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace diamondgold\DummyItemsBlocks;
+
+use InvalidArgumentException;
+use pocketmine\block\Block;
+use pocketmine\block\BlockBreakInfo;
+use pocketmine\block\BlockIdentifier;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\block\BlockTypeInfo;
+use pocketmine\block\Opaque;
+use pocketmine\block\RuntimeBlockStateRegistry;
+use pocketmine\data\bedrock\block\BlockStateData;
+use pocketmine\data\bedrock\block\BlockStateDeserializeException;
+use pocketmine\data\bedrock\block\convert\UnsupportedBlockStateException;
+use pocketmine\data\bedrock\item\SavedItemData;
+use pocketmine\inventory\CreativeInventory;
+use pocketmine\item\Item;
+use pocketmine\item\ItemBlock;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\item\StringToItemParser;
+use pocketmine\network\mcpe\convert\TypeConverter;
+use pocketmine\plugin\DisablePluginException;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\utils\AssumptionFailedError;
+use pocketmine\world\format\io\GlobalBlockStateHandlers;
+use pocketmine\world\format\io\GlobalItemDataHandlers;
+use Throwable;
+
+class Main extends PluginBase
+{
+    protected function onEnable(): void
+    {
+        $this->saveDefaultConfig();
+        $config = $this->getConfig();
+        //$config->set("blocks", (new \ReflectionClass(BlockTypeNames::class))->getConstants());
+        $removedBlocks = [
+            "minecraft:client_request_placeholder_block",
+            "minecraft:moving_block",
+        ];
+        $blocks = $config->get("blocks", []);
+        foreach ($blocks as $k => $id) {
+            if (in_array($id, $removedBlocks, true)) {
+                $this->getLogger()->warning("Block $id is intentionally removed!");
+                unset($blocks[$k]);
+            }
+            try {
+                if (GlobalBlockStateHandlers::getDeserializer()->deserializeBlock(BlockStateData::current($id, []))) {
+                    $this->getLogger()->warning("Block $id is already registered!");
+                    unset($blocks[$k]);
+                }
+            } catch (UnsupportedBlockStateException) {
+                // not registered
+            } catch (BlockStateDeserializeException $e) {
+                // registered but missing properties when deserializing
+                $this->getLogger()->debug($e->getMessage());
+                $this->getLogger()->warning("Block $id is already registered!");
+                unset($blocks[$k]);
+            }
+        }
+        //$config->set("items", (new \ReflectionClass(ItemTypeNames::class))->getConstants());
+        $items = $config->get("items", []);
+        foreach ($items as $k => $id) {
+            try {
+                if (StringToItemParser::getInstance()->parse($id)) {
+                    $this->getLogger()->warning("Item $id is in StringToItemParser!");
+                    unset($items[$k]);
+                }
+                if (GlobalItemDataHandlers::getDeserializer()->deserializeType(new SavedItemData($id))) {
+                    $this->getLogger()->warning("Item $id is already registered!");
+                    unset($items[$k]);
+                }
+            } catch (Throwable) {
+            }
+        }
+        $blocks = array_values($blocks);
+        $items = array_values($items);
+        if ($config->get("blocks") !== $blocks) {
+            $config->set("blocks", $blocks);
+        }
+        if ($config->get("items") !== $items) {
+            $config->set("items", $items);
+        }
+        if ($config->hasChanged()) {
+            $config->save();
+        }
+
+        self::registerBlocks($blocks);
+        self::registerItems($items);
+
+        // Server will crash if it tries to send these items to the client, blame PMMP
+        $changed = false;
+        foreach (CreativeInventory::getInstance()->getAll() as $item) {
+            try {
+                TypeConverter::getInstance()->coreItemStackToNet($item);
+            } catch (AssumptionFailedError) {
+                // Unmapped blockstate returned by blockstate serializer
+                if ($item instanceof ItemBlock) {
+                    $this->getLogger()->warning("Block {$item->getName()} is not supported");
+                    $key = array_search($item->getName(), $blocks, true);
+                    unset($blocks[$key]);
+                } else {
+                    $this->getLogger()->warning("Item {$item->getName()} is not supported");
+                    $key = array_search($item->getName(), $items, true);
+                    unset($items[$key]);
+                }
+                $changed = true;
+            } catch (Throwable $e) {
+                if ($item instanceof ItemBlock) {
+                    $this->getLogger()->warning("Block {$item->getName()} is not supported: " . $e->getMessage());
+                    $key = array_search($item->getName(), $blocks, true);
+                    unset($blocks[$key]);
+                } else {
+                    $this->getLogger()->warning("Item {$item->getName()} is not supported " . $e->getMessage());
+                    $key = array_search($item->getName(), $items, true);
+                    unset($items[$key]);
+                }
+                $changed = true;
+            }
+        }
+        if ($changed) {
+            $blocks = array_values($blocks);
+            $items = array_values($items);
+            $config->set("items", $items);
+            $config->set("blocks", $blocks);
+            $config->save();
+            $this->getLogger()->emergency("Server restart required to remove unsupported items");
+            throw new DisablePluginException();
+        }
+
+        $this->getServer()->getAsyncPool()->addWorkerStartHook(function (int $worker) use ($blocks, $items): void {
+            $this->getServer()->getAsyncPool()->submitTaskToWorker(new class(serialize($blocks), serialize($items)) extends AsyncTask {
+
+                public function __construct(private readonly string $blocksSerialized, private readonly string $itemsSerialized)
+                {
+
+                }
+
+                public function onRun(): void
+                {
+                    Main::registerBlocks(unserialize($this->blocksSerialized));
+                    Main::registerItems(unserialize($this->itemsSerialized));
+                }
+            }, $worker);
+        });
+    }
+
+    private static function generateNameFromId(string $id): string
+    {
+        $id = str_replace('minecraft:', '', $id);
+        $words = explode('_', $id);
+        $convertedText = '';
+        foreach ($words as $word) {
+            $convertedText .= ucfirst($word) . ' ';
+        }
+        return trim($convertedText);
+    }
+
+    /**
+     * @param string[] $blocks
+     * @return void
+     */
+    public static function registerBlocks(array $blocks): void
+    {
+        foreach ($blocks as $id) {
+            self::registerSimpleBlock($id, new Opaque(new BlockIdentifier(BlockTypeIds::newId()), self::generateNameFromId($id), new BlockTypeInfo(BlockBreakInfo::instant())), [$id]);
+        }
+    }
+
+    /**
+     * @param string[] $items
+     * @return void
+     */
+    public static function registerItems(array $items): void
+    {
+        foreach ($items as $id) {
+            self::registerSimpleItem($id, new Item(new ItemIdentifier(ItemTypeIds::newId()), self::generateNameFromId($id)), [$id]);
+        }
+    }
+
+    /**
+     * @param string[] $stringToItemParserNames
+     */
+    private static function registerSimpleBlock(string $id, Block $block, array $stringToItemParserNames): void
+    {
+        RuntimeBlockStateRegistry::getInstance()->register($block);
+
+        GlobalBlockStateHandlers::getDeserializer()->mapSimple($id, fn() => clone $block);
+        GlobalBlockStateHandlers::getSerializer()->mapSimple($block, $id);
+
+        foreach ($stringToItemParserNames as $name) {
+            StringToItemParser::getInstance()->registerBlock($name, fn() => clone $block);
+        }
+        CreativeInventory::getInstance()->add($block->asItem());
+    }
+
+    /**
+     * @param string[] $stringToItemParserNames
+     */
+    private static function registerSimpleItem(string $id, Item $item, array $stringToItemParserNames): void
+    {
+        GlobalItemDataHandlers::getDeserializer()->map($id, fn() => clone $item);
+        GlobalItemDataHandlers::getSerializer()->map($item, fn() => new SavedItemData($id));
+
+        foreach ($stringToItemParserNames as $name) {
+            try {
+                StringToItemParser::getInstance()->register($name, fn() => clone $item);
+            } catch (InvalidArgumentException) {
+                //var_dump("Already registered: $id $name");
+                return;
+            }
+        }
+        CreativeInventory::getInstance()->add($item);
+    }
+}
