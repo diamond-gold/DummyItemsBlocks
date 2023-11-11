447adc66aeaa625ca79238872cfaa7b707b6abfc
ace <diamond-gold-ace@outlook.com>
2023-11-11 19:43:56 +0800

    Specify enum defaults as class property default

--- src/diamondgold/DummyItemsBlocks/item/horn/GoatHorn.php
+++ src/diamondgold/DummyItemsBlocks/item/horn/GoatHorn.php
@@ -3,17 +3,10 @@
 namespace diamondgold\DummyItemsBlocks\item\horn;
 
 use pocketmine\item\Item;
-use pocketmine\item\ItemIdentifier;
 
 final class GoatHorn extends Item
 {
-    private GoatHornType $type;
-
-    public function __construct(ItemIdentifier $identifier, string $name)
-    {
-        $this->type = GoatHornType::PONDER;
-        parent::__construct($identifier, $name);
-    }
+    private GoatHornType $type = GoatHornType::PONDER;
 
     public function getType(): GoatHornType
     {
